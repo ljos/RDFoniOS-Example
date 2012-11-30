38d20ba6462e11ebb725311101928153738794bf
Bjarte Johansen <bjo013@uib.no>
2012-11-30 16:34:38 +0100

    add sparqlviewcontroller

---
+++ RDFoniOS/RDFSparqlQueryViewController.m
@@ -0,0 +1,41 @@
+//
+//  RDFSparqlQueryViewController.m
+//  RDFoniOS
+//
+//  Created by Bjarte Johansen on 11/30/12.
+//  Copyright (c) 2012 Bjarte Johansen. All rights reserved.
+//
+
+#import "RDFSparqlQueryViewController.h"
+
+@interface RDFSparqlQueryViewController () {
+    
+}
+
+
+@end
+
+@implementation RDFSparqlQueryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
