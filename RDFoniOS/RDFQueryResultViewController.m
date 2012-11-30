f83ace490837dc9b45e8f5747f089aa8f6c052e5
Bjarte Johansen <bjo013@uib.no>
2012-11-30 16:34:30 +0100

    add resultviewcontroller

---
+++ RDFoniOS/RDFQueryResultViewController.m
@@ -0,0 +1,36 @@
+//
+//  RDFQueryResultViewController.m
+//  RDFoniOS
+//
+//  Created by Bjarte Johansen on 11/30/12.
+//  Copyright (c) 2012 Bjarte Johansen. All rights reserved.
+//
+
+#import "RDFQueryResultViewController.h"
+
+@interface RDFQueryResultViewController () {
+    
+}
+
+
+@end
+
+@implementation RDFQueryResultViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+}
+
+@end
